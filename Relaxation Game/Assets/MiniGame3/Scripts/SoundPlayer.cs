c9df1651185ec07e55168bfb05012454cb6ceb9f
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-03-25 16:32:38 +0000

    sound game
    all sounds there make buttons nice, experiment with sounds of grottos

--- Relaxation Game/Assets/MiniGame3/Scripts/SoundPlayer.cs
+++ Relaxation Game/Assets/MiniGame3/Scripts/SoundPlayer.cs
@@ -8,6 +8,12 @@ public class SoundPlayer : MonoBehaviour
     [SerializeField] AudioClip sound0;
     [SerializeField] AudioClip sound1;
     [SerializeField] AudioClip sound2;
+    [SerializeField] AudioClip sound3;
+    [SerializeField] AudioClip sound4;
+    [SerializeField] AudioClip sound5;
+    [SerializeField] AudioClip sound6;
+    [SerializeField] AudioClip sound7;
+    [SerializeField] AudioClip sound8;
     [SerializeField] int soundClip;
 
     private void Start()
@@ -29,6 +35,30 @@ public class SoundPlayer : MonoBehaviour
                 break;
             case 2:
                 audioSource.clip = sound2;
+                audioSource.Play();
+                break;
+            case 3:
+                audioSource.clip = sound3;
+                audioSource.Play();
+                break;
+            case 4:
+                audioSource.clip = sound4;
+                audioSource.Play();
+                break;
+            case 5:
+                audioSource.clip = sound5;
+                audioSource.Play();
+                break;
+            case 6:
+                audioSource.clip = sound6;
+                audioSource.Play();
+                break;
+            case 7:
+                audioSource.clip = sound7;
+                audioSource.Play();
+                break;
+            case 8:
+                audioSource.clip = sound8;
                 audioSource.Play();
                 break;
         }

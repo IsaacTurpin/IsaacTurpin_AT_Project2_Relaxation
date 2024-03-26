f38da4f82621f5ae424117b357ee9b8051295ba4
IsaacTurpin <Isaac2.Turpin@live.uwe.ac.uk>
2024-03-26 16:24:48 +0000

    mg3 done
    add info text to mg2 and 3

--- Relaxation Game/Assets/MiniGame2/Scripts/Oscillator.cs
+++ Relaxation Game/Assets/MiniGame2/Scripts/Oscillator.cs
@@ -6,7 +6,6 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    //[SerializeField] [Range(0,1)] float movementFactor; - how to make something a slider in inspector
     float movementFactor;
     [SerializeField] float period = 2f;
     public bool begin;
@@ -116,23 +115,5 @@ public class Oscillator : MonoBehaviour
                 goLeft = true;
             }
         }
-    }
-
-    void old()
-    {
-
-
-
-        if (period <= Mathf.Epsilon) return; // Epsilon is a tiny number - the smallest possible float above 0
-
-        float cycles = Time.time / period; // continually growing over time
-
-        const float tau = Mathf.PI * 2; // constant value of 6.283
-        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
-
-        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so its cleaner
-
-        Vector3 offset = movementVector * movementFactor;
-        transform.localPosition = startingPosition + offset;
     }
 }
